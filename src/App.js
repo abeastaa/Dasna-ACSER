8a3c531b2c688195511c0718429e8f4a3558824c
Abeastaa <abeastaa@gmail.com>
2022-05-26 13:18:35 +0430

    handle restricted routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Suspense } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
 import './scss/style.scss'
 
 const loading = (
@@ -16,20 +16,31 @@ const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+
 class App extends Component {
   render() {
+    const token = false
+
     return (
-      <HashRouter>
+      <Router>
         <Suspense fallback={loading}>
           <Routes>
-            <Route exact path="/login" name="Login Page" element={<Login />} />
-            <Route exact path="/register" name="Register Page" element={<Register />} />
-            <Route exact path="/404" name="Page 404" element={<Page404 />} />
-            <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            <Route path="*" name="Home" element={<DefaultLayout />} />
+            {token ? (
+              <>
+                <Route exact path="/404" name="Page 404" element={<Page404 />} />
+                <Route exact path="/500" name="Page 500" element={<Page500 />} />
+                <Route exact path="*" element={<DefaultLayout />} />
+              </>
+            ) : (
+              <>
+                <Route path="/login" name="Login Page" element={<Login />} />
+                <Route path="/register" name="Register Page" element={<Register />} />
+                <Route path="*" element={<Navigate to="login" replace />} />
+              </>
+            )}
           </Routes>
         </Suspense>
-      </HashRouter>
+      </Router>
     )
   }
 }
