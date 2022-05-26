84800f44dbbece5238801120be6e447a31973d57
Abeastaa <abeastaa@gmail.com>
2022-05-26 10:48:35 +0430

    write dashboard back-end logic and modify useRequest with prettier rules

--- src/utility/useRequest.js
+++ src/utility/useRequest.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import axios from 'axios'
 
 export default function useRequest() {
-  
-    const jwtRequests = async (urlAddress, requestMethod = "get", body, headers, requestParams) => {
-        return new axios({
-            baseURL: "http://65.21.14.248:8089/",
-            url: urlAddress,
-            method: requestMethod,
-            data: body,
-            headers: {
-                "Content-Type": "application/json",
-                // Authorization: "Bearer",
-                ...headers
-            },
-            params: requestParams,
-            timeout: 1000 * 15
-        })
-    }
+  const jwtRequests = async (urlAddress, requestMethod = 'GET', body, headers, requestParams) => {
+    return new axios({
+      baseURL: 'http://65.21.14.248:8089/',
+      url: urlAddress,
+      method: requestMethod,
+      data: body,
+      headers: {
+        'Content-Type': 'application/json',
+        // Authorization: 'Bearer',
+        ...headers,
+      },
+      params: requestParams,
+      timeout: 1000 * 15,
+    })
+  }
+
+  return {
+    jwtRequests,
+  }
 }
