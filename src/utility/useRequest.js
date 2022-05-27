2486184094ab46ab8e0b964d74e36be110f64fca
Abeastaa <abeastaa@gmail.com>
2022-05-28 00:42:41 +0430

    refactor useRequest hook

--- src/utility/useRequest.js
+++ src/utility/useRequest.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import axios from 'axios'
 
+const getToken = async () => {
+  const token = JSON.parse(localStorage.getItem('accessToken'))
+  return token
+}
+
 export default function useRequest() {
-  const jwtRequests = async (urlAddress, requestMethod = 'GET', body, headers, requestParams) => {
+  const request = async (
+    token,
+    urlAddress,
+    requestMethod = 'GET',
+    body,
+    headers,
+    requestParams
+  ) => {
     return new axios({
-      baseURL: 'http://65.21.14.248:8089/',
+      baseURL: 'http://65.21.14.248:8088/',
       url: urlAddress,
       method: requestMethod,
       data: body,
       headers: {
         'Content-Type': 'application/json',
-        // Authorization: 'Bearer',
+        Authorization: token ? getToken() : null,
         ...headers,
       },
       params: requestParams,
@@ -19,6 +31,6 @@ export default function useRequest() {
   }
 
   return {
-    jwtRequests,
+    request,
   }
 }
